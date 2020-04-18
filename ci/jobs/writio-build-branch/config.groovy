7ff298aced392a1dbeb5f4c8dda3b2f47b5f2985
Christian Behrenberg <christian.behrenberg@gmail.com>
2020-04-18 22:06:09 +0200

    #7 added ptbranch param to job dsl config

--- ci/jobs/writio-build-branch/config.groovy
+++ ci/jobs/writio-build-branch/config.groovy
@@ -6,6 +6,22 @@ pipelineJob(jobName) {
 
     description("Builds a writio branch from remote GitHub repository '${repositoryUrl}'. When executed the first time, the ${defaultBranch}-branch will be built (default). Afterwards, you can select from a combo box all available other branches.")
 
+    parameters {
+        gitParameter {
+            name("BRANCH")
+            type("PT_BRANCH")
+            defaultValue("dev")
+            branch("dev")
+            description("writio branch to build")
+            branchFilter("origin/(.*)")
+            tagFilter("*")
+            sortMode("NONE")
+            selectedValue("NONE")
+            quickFilterEnabled(false)
+            useRepository("https://github.com/cbehrenberg/writio.git")
+        }
+    }
+
     properties {
 
         githubProjectUrl(repositoryUrl)
