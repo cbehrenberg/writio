08b427635f0f28cc7b66642661ed5b7e178f9916
Christian Behrenberg <christian.behrenberg@gmail.com>
2020-05-15 21:17:30 +0200

    #11 branch

--- ci/scripts/git-tag.sh
+++ ci/scripts/git-tag.sh
@@ -3,30 +3,30 @@
 # $1 : version
 # $2 : message
 
-echo "INFO: trying to create git tag ${1}..."
+echo "creating git tag ${1}..."
 
 if [ ! -z "$(git tag -l $1)" ]; then
-	echo "ERROR: git tag ${1} already exists!"
+	echo "git tag ${1} already exists!"
 	exit 1
 fi
 
-echo "INFO: executing: 'git tag -m \"${2}\" \"${1}\"'..."
+echo "executing: 'git tag -m \"${2}\" \"${1}\"'..."
 git tag -m "${2}" "${1}"
 
 if [ $? -eq 0 ]; then
-  echo "INFO: created git tag ${1} successfully!"
+  echo "created git tag ${1} successfully!"
 else
-  echo "ERROR: cannot create tag ${1}"
+  echo "cannot create tag ${1}!"
   exit 1
 fi
 
-echo "INFO: executing: 'git push origin ${1}'..."
+echo "executing: 'git push origin ${1}'..."
 git push origin "${1}"
 
 if [ $? -eq 0 ]; then
-  echo "INFO: pushed tag ${1} successfully to remote!"
+  echo "pushed tag ${1} successfully to remote!"
   exit 0
 else
-  echo "ERROR: cannot push git tag ${1} to remote!"
+  echo "cannot push git tag ${1} to remote!"
   exit 1
 fi
