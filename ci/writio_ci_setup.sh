2577d5a61b35f1d22af27d42dfb63740df2d7a30
Christian Behrenberg <christian.behrenberg@gmail.com>
2020-08-11 22:11:03 +0200

    #37 removed scripts, renamed writio ci yaml file and update setup sh

--- ci/writio_ci_setup.sh
+++ ci/writio_ci_setup.sh
@@ -1,14 +1,39 @@
 #!/bin/bash
 
-source ./scripts/ask.sh
+ask() {
+
+    local prompt default reply
+
+    if [ "${2:-}" = "Y" ]; then
+        prompt="Y/n"
+        default=Y
+    elif [ "${2:-}" = "N" ]; then
+        prompt="y/N"
+        default=N
+    else
+        prompt="y/n"
+        default=
+    fi
+
+    while true; do
+
+        echo -n "$1 [$prompt] "
+
+        read reply </dev/tty
+
+        if [ -z "$reply" ]; then
+            reply=$default
+        fi
+
+        case "$reply" in
+            Y*|y*) return 0 ;;
+            N*|n*) return 1 ;;
+        esac
+    done
+}
 
 echo "Asserting Docker being in swarm mode..."
-if [ "$(docker info | grep Swarm | sed 's/Swarm: //g')" == "inactive" ]; then
-	echo "Docker not in swarm mode, activating..."
-	docker swarm init
-else
-	echo "Docker already in swarm mode"
-fi
+docker swarm init
 
 echo "Creating secrets..."
 
